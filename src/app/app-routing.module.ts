55be5f8365d76654879a09e498b2dd5115b85701
alexisioa629 <alexisioa629@gmail.com>
2020-03-26 18:21:04 -0500

    Alexis-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './vistas/login/login.component';
+import {PagPrincipalComponent} from './vistas/pag-principal/pag-principal.component';
+import {ResponsablesComponent} from './vistas/componentes/responsables/responsables.component';
+import {ProyectosComponent} from './vistas/componentes/proyectos/proyectos.component';
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"pagPrincipal",component:PagPrincipalComponent, 
+      children:[
+        {path:"responsables",component:ResponsablesComponent},
+        {path:"proyectos",component:ProyectosComponent}
 
-
-const routes: Routes = [];
+  ]
+}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { PagPrincipalComponent } from './vistas/pag-principal/pag-principal.component';
+import { ProyectosComponent } from './vistas/componentes/proyectos/proyectos.component';
+import { ResponsablesComponent } from './vistas/componentes/responsables/responsables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagPrincipalComponent,
+    ProyectosComponent,
+    ResponsablesComponent
   ],
   imports: [
     BrowserModule,
